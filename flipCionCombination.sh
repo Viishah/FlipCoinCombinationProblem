7898d19aace13c522486bd972fb03b28d70dcacd
“Viishah” <vtorsalle@gmail.com>
2023-03-06 14:44:30 +0530

    Add: As a simulator, loop to create a Doublet Combination. Store the doublet combination as well as the percentage.

--- flipCionCombination.sh
+++ flipCionCombination.sh
@@ -1,17 +1,18 @@
 read -p "Enter number of times to flip a combination:" no_of_flips
+
+echo 
+
 declare -A singlet_frequency=( 
 
-                            [H]=0
-                            [T]=0
+                                [H]=0
+                                [T]=0
 
                               )
-echo 
-echo "---------------------------SINGLET-COMBINATION---------------------------"
 for(( flip = 1; flip <= no_of_flips; flip++ ))
 do
     echo -n "Flip-$flip is "
-    (( toss = RANDOM % 2 ))
-    case $toss in 
+    (( coin = RANDOM % 2 ))
+    case $coin in 
         0)
             echo "Heads"
             (( singlet_frequency[H]++ ))
@@ -20,12 +21,4 @@ do
             echo "Tails"
             (( singlet_frequency[T]++ ))
     esac
-done
-
-for combination in ${!singlet_frequency[@]}
-do
-    #update count values  with corresponding percentage
-    percentage=$(( ${singlet_frequency[$combination]}  * 100 / no_of_flips ))
-    singlet_frequency[$combination]=$percentage
-    echo "percentage of $combination is ${singlet_frequency[$combination]}%"   
 done
